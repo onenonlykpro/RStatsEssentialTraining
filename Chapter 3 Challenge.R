3ddb847429b94b6486360040084f1ddf748bdfcf
Kyle Protho <Onenonlykpro@gmail.com>
2016-11-21 15:06:14 -0700

    Have only requested stats show
    Updated end of code so that only the requested stats - mean, sd, skew, and kurtosis - are printed to the console.

--- Chapter 3 Challenge.R
+++ Chapter 3 Challenge.R
@@ -8,6 +8,10 @@ quarterMileTime <- mtcars$qsec
 
 # Calculate descriptive statistics for three variables. Miles per gallon, horsepower, and qsec.
 # For each of the three variables, calculate four the mean, the standard deviation, the skewness and kurtosis.
-describe(mpg)
-describe(horsepower)
-describe(quarterMileTime)
+mpgSummary <- describe(mpg)
+horsepowerSummary <- describe(horsepower)
+quarterMileTimeSummary <- describe(quarterMileTime)
+
+mpgSummary [, c(3,4,11,12)]
+horsepowerSummary [, c(3,4,11,12)]
+quarterMileTimeSummary [, c(3,4,11,12)]
